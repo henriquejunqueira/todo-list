4f79fe6ffcae2e99819daedd90839c7515c1f1c4
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-07-14 16:39:37 -0300

    desenvolvido todo list utilizando php, bootstrap, mysql

--- conexao.php
+++ conexao.php
@@ -2,20 +2,17 @@
   // ini_set('display_errors', 1);
   // ini_set('display_startup_errors', 1);
   // error_reporting(E_ALL);
-  
-  // $servername = "localhost";
-  // $username = "henrique";
-  // $password = "slipknot1994";
-  // $database = "todos";
 
-  $servername = "MYSQLHOST";
-  $username = "MYSQLUSER";
-  $password = "MYSQLPASSWORD";
-  $database = "MYSQLDATABASE";
-  $port = "MYSQLPORT";
+  $servername = getenv("MYSQLHOST");
+  $username = getenv("MYSQLUSER");
+  $password = getenv("MYSQLPASSWORD");
+  $database = getenv("MYSQLDATABASE");
+  $port = getenv("MYSQLPORT");
 
   // cria a conexão
   $connection = new mysqli($servername, $username, $password, $database, $port);
+  // $conn = "mysql://$username:$password@$servername:$port/$database";
+  // $connection = new mysqli($conn);
 
   // verifica conexão
   if($connection->connect_error){

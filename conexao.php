4710ac8d5f7478e2fac63f1093eab5577259ab22
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-07-14 23:19:49 -0300

    testando deploy

--- conexao.php
+++ conexao.php
@@ -3,6 +3,14 @@
   // ini_set('display_startup_errors', 1);
   // error_reporting(E_ALL);
 
+  require 'vendor/autoload.php';
+
+  // Carregar variáveis de ambiente (no desenvolvimento local)
+  if (file_exists(__DIR__ . '/.env')) {
+      $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+      $dotenv->load();
+  }
+
   $servername = getenv("SERVERNAME");
   $username = getenv("USERNAME");
   $password = getenv("PASSWORD");
@@ -16,3 +24,8 @@
   if($connection->connect_error){
     die("Falha na conexão: " . $connection->connect_error);
   }
+
+  // Teste simples de consulta ao banco de dados (opcional)
+  $result = $mysqli->query("SELECT NOW()");
+  $row = $result->fetch_assoc();
+  echo "Current time: " . $row['NOW()'];
